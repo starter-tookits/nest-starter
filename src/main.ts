b2c75d3c0998597d91a91ec3eeeb6b7be2a6aa5f
aquariuslt <superaquariuslt@gmail.com>
2019-08-28 12:50:43 +0800

    chore: initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+
+import * as compression from 'compression';
+import { AppModule } from '@/app.module';
+
+async function bootstrap(): Promise<void> {
+  // API_PORT will be inject when build docker image but not from .dotenv
+  const port = process.env.API_PORT || 5000;
+  const app = await NestFactory.create(AppModule);
+
+  app.use(compression());
+  await app.listen(port);
+}
+
+bootstrap().then();
