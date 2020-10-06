02151335b13d3671f5d5eae2ac4fb03d574b4672
Aquariuslt <superaquariuslt@gmail.com>
2020-10-07 00:18:19 +0800

    v2/bump dependencies (#2)
    * refactor: bump deps and update schematics
    
    * refactor: bump deps and update schematics
    
    - add log4js module
    - add config dependencies
    - add swagger dependencies
    - add terminus dependencies

---
+++ README.md
@@ -0,0 +1,9 @@
+# nest-starter
+
+nestjs starter template with below bundled features
+
+
+- [x] with validated schema config (@nestjs/config)
+- [x] with logger (@nestx-log4js/core)
+- [x] with postgresql (@nestjs/typeorm + pg)
+- [x] with swagger api
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
-
-import * as compression from 'compression';
-import { AppModule } from '@/app.module';
+import { AppModule } from './app.module';
+import { Log4jsLogger } from '@nestx-log4js/core';
 
 async function bootstrap(): Promise<void> {
   // API_PORT will be injected when build docker image but not from dotenv
   const port = process.env.API_PORT || 5000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: false
+  });
 
-  app.use(compression());
+  app.useLogger(app.get(Log4jsLogger));
+
   await app.listen(port);
 }
 
