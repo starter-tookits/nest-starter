28c2cd829f7816424a407993e795727379c8d775
aquariuslt <superaquariuslt@gmail.com>
2020-10-06 20:34:40 +0800

    refactor: bump deps and update schematics

---
+++ README.md
@@ -0,0 +1,9 @@
+# nest-starter
+
+nestjs starter template with below bundled features
+
+
+- [x] centralize config
+- [x] with logger (@nestx-log4js/core)
+- [x] with postgresql (@nestjs/typeorm + pg)
+- [x] with swagger api
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-
-import * as compression from 'compression';
-import { AppModule } from '@/app.module';
+import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   // API_PORT will be injected when build docker image but not from dotenv
   const port = process.env.API_PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
-  app.use(compression());
   await app.listen(port);
 }
 
